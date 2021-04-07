e3902f15220377eaed5efedc85dadb342cee10da
parora07234 <78341157+parora07234@users.noreply.github.com>
2021-04-07 17:23:40 +0530

    Update DataGen.py

--- code/DataGen.py
+++ code/DataGen.py
@@ -44,106 +44,106 @@ class DataGenerator:
 		y_ideal_mean = np.ones_like(X_ideal)+0.5
 # 		nonlocal X_train, y_train, X_val, y_val ## ADDED BY PARUL
 		
-		if self.type_in=="drunk_bow_tie":
-			"""
-			similar to bow tie but less linear
-			"""	
+# 		if self.type_in=="drunk_bow_tie":
+# 			"""
+# 			similar to bow tie but less linear
+# 			"""	
 		
-			X = np.random.uniform(low=-2.,high=2.,size=(n_samples,1))
-			y = 1.5*np.sin(np.pi*X[:,0]) + np.random.normal(loc=0.,scale=1.*np.power(X[:,0],2))
-			y = y.reshape([-1,1])/5.
-			X_train = X
-			y_train = y	
+# 			X = np.random.uniform(low=-2.,high=2.,size=(n_samples,1))
+# 			y = 1.5*np.sin(np.pi*X[:,0]) + np.random.normal(loc=0.,scale=1.*np.power(X[:,0],2))
+# 			y = y.reshape([-1,1])/5.
+# 			X_train = X
+# 			y_train = y	
 
-			X = np.random.uniform(low=-2.,high=2.,size=(int(10*n_samples),1))
-			y = 1.5*np.sin(np.pi*X[:,0]) + np.random.normal(loc=0.,scale=1.*np.power(X[:,0],2))
-			y = y.reshape([-1,1])/5.		
-			X_val = X
-			y_val = y
+# 			X = np.random.uniform(low=-2.,high=2.,size=(int(10*n_samples),1))
+# 			y = 1.5*np.sin(np.pi*X[:,0]) + np.random.normal(loc=0.,scale=1.*np.power(X[:,0],2))
+# 			y = y.reshape([-1,1])/5.		
+# 			X_val = X
+# 			y_val = y
 
-			y_ideal_U = 1.5*np.sin(np.pi*X_ideal) + n_std_devs*np.power(X_ideal,2)
-			y_ideal_U = y_ideal_U/5.
-			y_ideal_L = 1.5*np.sin(np.pi*X_ideal) - n_std_devs*np.power(X_ideal,2)
-			y_ideal_L = y_ideal_L/5.
-			y_ideal_mean = 1.5*np.sin(np.pi*X_ideal)
-			y_ideal_mean = y_ideal_mean/5.	
+# 			y_ideal_U = 1.5*np.sin(np.pi*X_ideal) + n_std_devs*np.power(X_ideal,2)
+# 			y_ideal_U = y_ideal_U/5.
+# 			y_ideal_L = 1.5*np.sin(np.pi*X_ideal) - n_std_devs*np.power(X_ideal,2)
+# 			y_ideal_L = y_ideal_L/5.
+# 			y_ideal_mean = 1.5*np.sin(np.pi*X_ideal)
+# 			y_ideal_mean = y_ideal_mean/5.	
 
-			# overwrite for convenience!
-			X_val = X_train
-			y_val = y_train
+# 			# overwrite for convenience!
+# 			X_val = X_train
+# 			y_val = y_train
 
-		elif self.type_in=="drunk_bow_tie_exp":
-			"""
-			similar to bow tie but less linear, now with non-gaussian noise
-			"""	
+# 		elif self.type_in=="drunk_bow_tie_exp":
+# 			"""
+# 			similar to bow tie but less linear, now with non-gaussian noise
+# 			"""	
 
-			X = np.random.uniform(low=-2.,high=2.,size=(n_samples,1))
-			y = 1.5*np.sin(np.pi*X[:,0]) + np.random.exponential(scale=1.*np.power(X[:,0],2))
-			y = y.reshape([-1,1])/5.
-			X_train = X
-			y_train = y	
+# 			X = np.random.uniform(low=-2.,high=2.,size=(n_samples,1))
+# 			y = 1.5*np.sin(np.pi*X[:,0]) + np.random.exponential(scale=1.*np.power(X[:,0],2))
+# 			y = y.reshape([-1,1])/5.
+# 			X_train = X
+# 			y_train = y	
 
-			X = np.random.uniform(low=-2.,high=2.,size=(int(10*n_samples),1))
-			y = 1.5*np.sin(np.pi*X[:,0]) + np.random.exponential(scale=1.*np.power(X[:,0],2))
-			y = y.reshape([-1,1])/5.		
-			X_val = X
-			y_val = y
+# 			X = np.random.uniform(low=-2.,high=2.,size=(int(10*n_samples),1))
+# 			y = 1.5*np.sin(np.pi*X[:,0]) + np.random.exponential(scale=1.*np.power(X[:,0],2))
+# 			y = y.reshape([-1,1])/5.		
+# 			X_val = X
+# 			y_val = y
 
-			# for exponential quantile = ln(1/quantile) /lambda
-			# note that np inputs beta = 1/lambda
-			y_ideal_U = 1.5*np.sin(np.pi*X_ideal) + np.log(1/(1-0.95))*np.power(X_ideal,2)
-			y_ideal_U = y_ideal_U/5.
-			y_ideal_L = 1.5*np.sin(np.pi*X_ideal)
-			y_ideal_L = y_ideal_L/5.
-			y_ideal_mean = 1.5*np.sin(np.pi*X_ideal)
-			y_ideal_mean = y_ideal_mean/5.	
+# 			# for exponential quantile = ln(1/quantile) /lambda
+# 			# note that np inputs beta = 1/lambda
+# 			y_ideal_U = 1.5*np.sin(np.pi*X_ideal) + np.log(1/(1-0.95))*np.power(X_ideal,2)
+# 			y_ideal_U = y_ideal_U/5.
+# 			y_ideal_L = 1.5*np.sin(np.pi*X_ideal)
+# 			y_ideal_L = y_ideal_L/5.
+# 			y_ideal_mean = 1.5*np.sin(np.pi*X_ideal)
+# 			y_ideal_mean = y_ideal_mean/5.	
 
-			X_val = X_train
-			y_val = y_train
+# 			X_val = X_train
+# 			y_val = y_train
 
-		elif self.type_in=="periodic_1":
-			"""
-			creates a bow tie shape with changing variance
-			"""
-			X = np.random.uniform(low=-5.,high=5.,size=(n_samples,self.n_feat))
-			y = 2.1*np.cos(0.2*X[:,0]) + 0.7*np.cos(20.1*X[:,0]) + 0.2*np.cos(10.4*X[:,0]) + np.random.normal(loc=0.,scale=0.1*np.ones_like(X[:,0]))
-			y = y.reshape([-1,1])/1.
-			X_train = X
-			y_train = y	
-			X_val = X_train
-			y_val = y_train
-			# y_ideal_U = X_ideal/5. + n_std_devs * np.abs(X_ideal)/5.
-			# y_ideal_L = X_ideal/5. - n_std_devs * np.abs(X_ideal)/5.
+# 		elif self.type_in=="periodic_1":
+# 			"""
+# 			creates a bow tie shape with changing variance
+# 			"""
+# 			X = np.random.uniform(low=-5.,high=5.,size=(n_samples,self.n_feat))
+# 			y = 2.1*np.cos(0.2*X[:,0]) + 0.7*np.cos(20.1*X[:,0]) + 0.2*np.cos(10.4*X[:,0]) + np.random.normal(loc=0.,scale=0.1*np.ones_like(X[:,0]))
+# 			y = y.reshape([-1,1])/1.
+# 			X_train = X
+# 			y_train = y	
+# 			X_val = X_train
+# 			y_val = y_train
+# 			# y_ideal_U = X_ideal/5. + n_std_devs * np.abs(X_ideal)/5.
+# 			# y_ideal_L = X_ideal/5. - n_std_devs * np.abs(X_ideal)/5.
 
-		elif self.type_in=="x_cubed_gap":
-			"""
-			toy data problem from Probabilistic Backprop (Lobato) & 
-			deep ensembles (Blundell)
-			but added gap here
+# 		elif self.type_in=="x_cubed_gap":
+# 			"""
+# 			toy data problem from Probabilistic Backprop (Lobato) & 
+# 			deep ensembles (Blundell)
+# 			but added gap here
 
-			"""
-			scale_c = 50.
-			half_samp = int(round(n_samples/2))
-			X_1 = np.random.uniform(low=-4.,high=-1.,size=(half_samp,1))
-			X_2 = np.random.uniform(low=1.,high=4.,size=(n_samples - half_samp,1))
-			X = np.concatenate((X_1, X_2))
-			y = X[:,0]**3 + np.random.normal(loc=0.,scale=3., size=X[:,0].shape[0])
-			y = y.reshape([-1,1])/scale_c
-			X_train = X
-			y_train = y			
-			X_val = X_train
-			y_val = y_train
+# 			"""
+# 			scale_c = 50.
+# 			half_samp = int(round(n_samples/2))
+# 			X_1 = np.random.uniform(low=-4.,high=-1.,size=(half_samp,1))
+# 			X_2 = np.random.uniform(low=1.,high=4.,size=(n_samples - half_samp,1))
+# 			X = np.concatenate((X_1, X_2))
+# 			y = X[:,0]**3 + np.random.normal(loc=0.,scale=3., size=X[:,0].shape[0])
+# 			y = y.reshape([-1,1])/scale_c
+# 			X_train = X
+# 			y_train = y			
+# 			X_val = X_train
+# 			y_val = y_train
 
-			y_ideal_U = X_ideal**3 + n_std_devs*3.
-			y_ideal_U = y_ideal_U/scale_c
-			y_ideal_L = X_ideal**3 - n_std_devs*3.
-			y_ideal_L = y_ideal_L/scale_c
-			y_ideal_mean = X_ideal**3
-			y_ideal_mean = y_ideal_mean/scale_c
+# 			y_ideal_U = X_ideal**3 + n_std_devs*3.
+# 			y_ideal_U = y_ideal_U/scale_c
+# 			y_ideal_L = X_ideal**3 - n_std_devs*3.
+# 			y_ideal_L = y_ideal_L/scale_c
+# 			y_ideal_mean = X_ideal**3
+# 			y_ideal_mean = y_ideal_mean/scale_c
 
 		# use single char '~' at start to identify real data sets
-		elif self.type_in[:1] == '~':
-
+# 		elif self.type_in[:1] == '~':
+		if self.type_in[:1] == '~':
 			if self.type_in=="~boston":
 				path = 'boston_housing_data.csv'
 				data = np.loadtxt(path,skiprows=0)
@@ -152,7 +152,7 @@ class DataGenerator:
 				data = np.loadtxt(path, delimiter=',',skiprows=1)
 			elif self.type_in=="~wind":
 				path = '/content/Deep_Learning_Prediction_Intervals/code/canada_CSV.csv'
-				data = np.loadtxt(path,delimiter=',',skiprows=1,usecols = (1,2)) ## CHECK WHTHER TO HAVE LOADTXT OR ANYTHING ELSE
+				data = np.loadtxt(path,delimiter=',',skiprows=1,usecols = (1,2)) ## CHECK WHTHER TO HAVE LOADTXT OR ANYTHING ELSE PARUL
 			
 			
 			# work out normalisation constants (need when unnormalising later)
@@ -194,7 +194,7 @@ class DataGenerator:
 		return X_train, y_train, X_val, y_val
 
 
-	def ViewData(self, n_rows=5, hist=False, plot=False, print_=True):
+	def ViewData(self, n_rows=5, hist=True, plot=True, print_=True):#plot and hist changed by PARUL
 		"""
 		print first few rows of data
 		option to view histogram of x and y
