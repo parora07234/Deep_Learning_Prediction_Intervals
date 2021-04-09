30d18de7bac7a5a4f2ccc82aa48a3e79a21f5d26
parora07234 <78341157+parora07234@users.noreply.github.com>
2021-04-09 09:35:10 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -97,8 +97,7 @@ run=0
 for run in range(0,n_runs):
 	# generate data
 	Gen = DataGenerator(type_in=type_in)	
-	X_train, y_train, X_val, y_val = Gen.CreateData(n_samples=n_samples,seed_in=run,
-		train_prop=train_prop, bound_limit=bound_limit, n_std_devs=n_std_devs)
+	X_train, y_train, X_val, y_val = Gen.CreateData(n_samples=n_samples,seed_in=run,train_prop=train_prop, bound_limit=bound_limit, n_std_devs=n_std_devs)
 
 	print('\n--- view data ---')
 	
