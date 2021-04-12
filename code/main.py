65d56fbe0638275b81842a91b30a7eefc5526f50
parora07234 <78341157+parora07234@users.noreply.github.com>
2021-04-12 10:21:57 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -28,13 +28,13 @@ loss_type = 'qd_soft' 		# loss type to train on - qd_soft mve mse (mse=simple po
 n_samples = 100		# if generating data, how many points to generate
 h_size = [100]	# number of hidden units in network: [50]=layer_1 of 50, [8,4]=layer_1 of 8, layer_2 of 4
 alpha = 0.05		# data points captured = (1 - alpha) ## FOR 95% PREDICTION INTERVAL
-n_epoch = 3000	# number epochs to train for
+n_epoch = 4000	# number epochs to train for
 optim = 'adam' 		# opitimiser - SGD adam ## ITS GRADIENT DESCENT BASED OPTIMISER 
 l_rate = 0.01        # learning rate of optimiser
 decay_rate=0.95 	# learning rate decay
-soften = 170. 		# hyper param for QD_soft
+soften = 160. 		# hyper param for QD_soft
 lambda_in = 15. 	# hyper param for QD_soft
-sigma_in=0.2		# initialise std dev of NN weights
+sigma_in=0.4		# initialise std dev of NN weights
 is_run_test=True	# if averaging over lots of runs - turns off some prints and graphs
 n_ensemble=5		# number of individual NNs in ensemble ## CHECK
 n_bootstraps=1 		# how many boostrap resamples to perform  ## pick one sample and again put ANOTHER SAMPLE
