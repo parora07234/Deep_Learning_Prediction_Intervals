9c3cf00740037dcfbddda87d2b792572556b9354
parora07234 <78341157+parora07234@users.noreply.github.com>
2021-04-12 10:01:40 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -26,13 +26,13 @@ type_in = 'wind' 	# data type to use - drunk_bow_tie x__gap ~boston concrete ,
 				##CHANGE THE DATA TYPE AS PER UR USE
 loss_type = 'qd_soft' 		# loss type to train on - qd_soft mve mse (mse=simple point prediction) ## CHECK FOR DIFFERENT LOSSES (gauss_like,qd_soft,mse)
 n_samples = 100		# if generating data, how many points to generate
-h_size = [50]	# number of hidden units in network: [50]=layer_1 of 50, [8,4]=layer_1 of 8, layer_2 of 4
+h_size = [100]	# number of hidden units in network: [50]=layer_1 of 50, [8,4]=layer_1 of 8, layer_2 of 4
 alpha = 0.05		# data points captured = (1 - alpha) ## FOR 95% PREDICTION INTERVAL
 n_epoch = 2000	# number epochs to train for
 optim = 'adam' 		# opitimiser - SGD adam ## ITS GRADIENT DESCENT BASED OPTIMISER 
-l_rate = 0.02          # learning rate of optimiser
-decay_rate=0.85   	# learning rate decay
-soften = 160. 		# hyper param for QD_soft
+l_rate = 0.01        # learning rate of optimiser
+decay_rate=0.98  	# learning rate decay
+soften = 150. 		# hyper param for QD_soft
 lambda_in = 15. 	# hyper param for QD_soft
 sigma_in=0.4 		# initialise std dev of NN weights
 is_run_test=True	# if averaging over lots of runs - turns off some prints and graphs
